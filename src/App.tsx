755360c7fcaf95bb8f7e5709785dd2b6b22f9c9f
Paula Penedo <paulinhapenedo@gmail.com>
2024-03-24 18:33:27 -0300

    fix: adjust Header and App component to accommodate footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { PropertyList } from "./components/PropertyList";
 
 function App() {
   return (
-    <>
+    <main className="flex flex-col min-h-dvh">
       <Header />
-    </>
+      <PropertyList />
+      <Footer />
+    </main>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { strings } from "./strings";
 
 export function Header() {
   return (
-    <header className="sticky top-0 border-b border-slate-950 px-4 md:px-8">
+    <header className="sticky top-0 border-b border-slate-950 px-4 md:px-8 bg-white z-10">
       <div className="border-x border-slate-950 w-full h-24 flex items-center px-6">
         <a
           href="/"
