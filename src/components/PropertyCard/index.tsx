1e32f362c9b40cfd3d0a9440e3a614993389a077
Paula Penedo <paulinhapenedo@gmail.com>
2024-03-25 16:29:44 -0300

    feat: only show edit/cancel buttons if there's a reservation

--- src/components/PropertyCard/index.tsx
+++ src/components/PropertyCard/index.tsx
@@ -1,15 +1,22 @@
 import { Star } from "lucide-react";
 
-import { formatPriceToCurrency } from "@/lib/currency";
-import { Property } from "@/types";
 import { Button } from "@/components/ui/button";
+import { formatPriceToCurrency } from "@/lib/currency";
+import { useBookingStore } from "@/stores/booking";
+import { Property } from "@/types";
 import { CreateReservationModal } from "../CreateReservationModal";
+import { strings } from "./strings";
 
 interface PropertyCardProps {
   property: Property;
 }
 
 export function PropertyCard({ property }: PropertyCardProps) {
+  const { bookings } = useBookingStore();
+
+  const hasReservations =
+    bookings.filter((booking) => booking.id === property.id).length > 0;
+
   return (
     <article className="border border-slate-950 px-6 py-4 shadow-black shadow-[5px_5px_0px_0px_rgba(0,0,0)]">
       <div className="w-full aspect-[5/4] md:aspect-[3/4] relative border border-slate-950 mb-3">
@@ -28,7 +35,7 @@ export function PropertyCard({ property }: PropertyCardProps) {
         <div className="flex justify-between items-center">
           <p className="font-semibold">
             {formatPriceToCurrency(property.price)}{" "}
-            <span className="font-normal">/night</span>
+            <span className="font-normal">{strings.perNight}</span>
           </p>
 
           <div className="font-semibold flex items-center gap-1">
@@ -36,11 +43,14 @@ export function PropertyCard({ property }: PropertyCardProps) {
             <span className="text-sm">{property.rating}</span>
           </div>
         </div>
-        <div className="flex flex-wrap gap-2">
-          <CreateReservationModal property={property} />
-          <Button>Edit reservation</Button>
-          <Button variant="destructive">Cancel reservation</Button>
-        </div>
+        <CreateReservationModal property={property} />
+        {hasReservations && (
+          <div className="flex flex-wrap gap-2">
+            <p className="text-sm ">{strings.changeReservation}</p>
+            <Button variant="outline">Edit</Button>
+            <Button variant="destructive">Cancel</Button>
+          </div>
+        )}
       </div>
     </article>
   );
---
+++ src/components/PropertyCard/strings.ts
@@ -0,0 +1,5 @@
+export const strings = {
+  perNight: "/night",
+  changeReservation:
+    "Seems like you have a reservation. What do you want do to?",
+};
